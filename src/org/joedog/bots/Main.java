568207407bca9c95f4f106b4c25189d3b5ad798c
JoeDog <jeff@joedog.org>
2014-07-24 09:44:37 -0400

    Moved from an applet to an application

--- src/org/joedog/bots/Main.java
+++ src/org/joedog/bots/Main.java
@@ -1,68 +1,58 @@
 package org.joedog.bots;
 
-import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Event;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.awt.Graphics;
+
+import java.awt.Rectangle;
+
 import java.awt.image.BufferedImage;
 
+import org.joedog.bots.control.Game;
 import org.joedog.bots.control.GameEngine;
+import org.joedog.bots.control.GameThread;
 import org.joedog.bots.model.Config;
 import org.joedog.bots.model.Location;
 
-public class Main extends Applet implements Runnable, MouseListener {
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.WindowConstants;
+
+public class Main extends JPanel implements MouseListener {
   private static final long serialVersionUID = -2666347118493388423L;
-  private GameEngine engine = new GameEngine();
+  private static GameEngine engine = null;
+  private static Canvas     canvas = null;
 
-  public void init() {
+  public Main(Canvas canvas) {   
+    super(new BorderLayout());
+    canvas.addMouseListener(this);
     this.addMouseListener(this);
-    
-    this.setFocusable(true);
   }
 
-  public void start() {
-    new Thread(this).start();
-  }
-
-  public void run() {
-    
-    setSize(Config.WIDTH, Config.HEIGHT+50); // for appletviewer (remove)
-
-    BufferedImage screen = new BufferedImage(Config.WIDTH, Config.HEIGHT+50, BufferedImage.TYPE_INT_RGB);
-    Graphics g = screen.getGraphics();
-    Graphics appletGraphics = getGraphics();
-
-    long delta = 0l;
-   
-    int x = 0; 
-    while (true) {
-      long lastTime = System.nanoTime();
-
-      g.setColor(new Color(205, 201, 201));
-      g.fillRect(0, 0, Config.WIDTH, Config.HEIGHT);
-
-      engine.update((float)(delta / 1000000000.0));
-      engine.render(g);
-      
-      appletGraphics.drawImage(screen, 0, 0, null);
-
-      delta = System.nanoTime() - lastTime;
-      //if (x == 25) engine.clear();
-      if (delta < 20000000L) {
-        try {
-          Thread.sleep((20000000L - delta) / 1000000L);
-        } catch (Exception e) {
-          // It's an interrupted exception, and nobody cares
-        }
-      }
-
-      if (!isActive()) {
-        return;
-      }
-      x = (x < 26) ? x+1 : 0;
-    }
+  private static void createAndShowGui(Canvas canvas) {
+    final JFrame frame = new JFrame("Bully And The Bots");
+    JComponent   panel = new Main(canvas);
+    panel.add(canvas, BorderLayout.CENTER);
+    canvas.setFocusable(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setContentPane(panel); 
+    frame.setPreferredSize(new Dimension(Config.WIDTH+16, Config.HEIGHT+94));
+    frame.setSize(Config.WIDTH+16, Config.HEIGHT+94);
+    frame.pack();
+    frame.setLocation(10, 10);
+    frame.setVisible(true);
+    Rectangle r = frame.getBounds();
+    System.out.println("W: "+r.width+" H: "+r.height);
   }
 
   public void mousePressed(MouseEvent e){
@@ -78,4 +68,54 @@ public class Main extends Applet implements Runnable, MouseListener {
   public void mouseClicked(MouseEvent e) {} 
   public void mouseEntered(MouseEvent e) {}
   public void mouseExited(MouseEvent e) {}
+
+  private static void adjustLAF() throws ClassNotFoundException,
+    InstantiationException, IllegalAccessException,
+    UnsupportedLookAndFeelException {
+    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+      if ("Nimbus".equals(info.getName())) {
+        //UIManager.setLookAndFeel(info.getClassName());
+        break;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    if (engine == null) {
+      engine = new GameEngine();
+    }
+
+    if (canvas == null) {
+      canvas = new Canvas();
+    }
+
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      adjustLAF();
+    }
+    catch (javax.swing.UnsupportedLookAndFeelException e) {
+      System.err.println("Bully requires java-1.6 or higher");
+    }
+    catch (ClassNotFoundException e) {
+      System.err.println("Bully requires java-1.6 or higher");
+    }
+    catch (InstantiationException e) {
+    }
+    catch (IllegalAccessException e) {
+    }
+
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        createAndShowGui(canvas);
+      }
+    });
+
+    while (true) {
+      Game game = new Game(engine, canvas);
+      GameThread thread = new GameThread(game);
+      engine.addThread(thread);
+      thread.start();
+      while (thread.isAlive()) ;
+    }
+  }
 }
