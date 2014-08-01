e47aa81b8a9751577e6a2a84e0ca45f1dc23adc4
JoeDog <jeff@joedog.org>
2014-08-01 11:06:54 -0400

    Provided sleep granularity

--- src/org/joedog/util/Sleep.java
+++ src/org/joedog/util/Sleep.java
@@ -1,14 +1,64 @@
 package org.joedog.util;
 
 public final class Sleep {
-  
-  public static void sleep(int i) {
-    nap(i);
+ 
+  /**
+   * Puts the thread to sleep (cease operation) 
+   * for n milliseconds, subject to the precision 
+   * and accuracy of system timers and schedulers.
+   * <p>
+   * @param  long   A duration in milliseconds.
+   * @return void   
+   * @see    Thread.sleep
+   */ 
+  public static void milliseconds(long n) {
+    nap(n);
   }
 
-  public static void nap(int i) {
+  /**
+   * Puts the thread to sleep (cease operation)
+   * for n seconds, subject to the precision
+   * and accuracy of system timers and schedulers.
+   * <p>
+   * @param  long   A duration in seconds.
+   * @return void
+   * @see    Thread.sleep
+   */
+  public static void seconds(long n) {
+    nap(n*1000);
+  }
+
+  /**
+   * Puts the thread to sleep (cease operation)
+   * for n minutes, subject to the precision
+   * and accuracy of system timers and schedulers.
+   * <p>
+   * @param  long   A duration in minutes.
+   * @return void
+   * @see    Thread.sleep
+   */
+  public static void minutes(long n) {
+    nap(n*60000);
+  }
+
+  /**
+   * Puts the thread to sleep (cease operation)
+   * for n hours, subject to the precision
+   * and accuracy of system timers and schedulers.
+   * <p>
+   * @param  long   A duration in hours.
+   * @return void
+   * @see    Thread.sleep
+   */
+  public static void hours(long n) {
+    nap(n*3600000);
+  }
+
+  private static void nap(long n) {
     try {
-      Thread.sleep(i);
-    } catch (Exception e) {}
+      Thread.sleep(n);
+    } catch (Exception e) {
+      // It's an interrupted exception; no one cares....
+    }
   }
 }
